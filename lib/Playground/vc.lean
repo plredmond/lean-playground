8e9e54dc4ce61dea78a3e52c71530eeb7a78a91e
PLR <51248199+plredmond@users.noreply.github.com>
2025-01-21 12:21:30 -0800

    update vc.lean to use mathlib4 toolchain

--- lib/Playground/vc.lean
+++ lib/Playground/vc.lean
@@ -1,8 +1,10 @@
-axiom size_zipWith {α β γ} (as : Array α) (bs : Array β) (f : α → β → γ) :
-    (as.zipWith bs f).size = min as.size bs.size
-    -- FIXME use the version of this in stdlib (batteries)
+import Init.Data.Array.Lemmas
 
-theorem size_set (a : Array α) i v : (a.set i v).size = a.size :=
+axiom Array.local_size_zipWith {α β γ} (as : Array α) (bs : Array β) (f : α → β → γ) :
+    (as.zipWith bs f).size = min as.size bs.size
+
+theorem Array.local_size_set {α} (a : Array α) (i : Fin a.size) v :
+    (a.set i v).size = a.size :=
     by simp
 
 -- -- --
@@ -18,31 +20,30 @@ structure VectorClock (n : Nat) : Type where
     isSized : vc.size = n
     deriving Repr
 
-def VectorClock.empty : VectorClock n :=
+def VectorClock.empty {n} : VectorClock n :=
     { vc := (List.replicate n 0).toArray
     , isSized := by simp
     }
 
+-- "@" symbol indicates that all the implicits will be provided explicitly
 #eval @VectorClock.empty 3
 
-def VectorClock.tick (v : VectorClock n) (p : Pid n) : VectorClock n :=
-    -- FIXME kyle miller says to use Fin.cast (mathlib4) for this b/c it has
-    -- more theorems around it than ▸ and cast
-    let i := v.isSized.symm ▸ p.pid
+def VectorClock.tick {n} (v : VectorClock n) (p : Pid n) : VectorClock n :=
+    let i := Fin.cast v.isSized.symm p.pid -- kyle miller says to use Fin.cast (mathlib4) for this b/c it has more theorems around it than ▸ and cast
     let c := v.vc[i].succ
     { v with
         vc := v.vc.set i c
-        isSized := (v.vc.size_set i c).trans v.isSized
+        isSized := (v.vc.size_set i c _).trans v.isSized
     }
 
-#eval (@VectorClock.empty 3).tick (Pid.mk 0)
+#eval (@VectorClock.empty 3).tick (Pid.mk 1)
 
-def VectorClock.merge (a b : VectorClock n) : VectorClock n :=
+def VectorClock.merge {n} (a b : VectorClock n) : VectorClock n :=
     { vc := a.vc.zipWith b.vc max
-    , isSized := by simp [size_zipWith a.vc b.vc _, a.isSized, b.isSized]
+    , isSized := by simp [Array.size_zipWith a.vc b.vc _, a.isSized, b.isSized]
     }
 
-instance : LE (VectorClock n) where
+instance {n} : LE (VectorClock n) where
     le a b := sorry -- need a ∧ but for a sequence?
 #check Array.all
 
@@ -56,7 +57,7 @@ structure Process (n : Nat) : Type where
     pid : Pid n
     vc : VectorClock n
 
-def Process.new (p : Pid n) : Process n :=
+def Process.new {n} (p : Pid n) : Process n :=
     { pid := p
     , vc := VectorClock.empty
     -- TODO delay queue
